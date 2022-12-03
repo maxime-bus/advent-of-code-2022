abe5d92d3ff642877ef7eb064ee1bba679b712b0
Maxime BUS <bus.maxime@mailoo.org>
2022-12-03 10:13:19 +0100

    [day03] part 2

--- day-03/app/Main.hs
+++ day-03/app/Main.hs
@@ -1,14 +1,10 @@
 module Main where
 
+import Data.Char (isUpper, ord)
 import Data.List (intersect)
-import Data.Char (ord, isUpper)
 
-stringToRuckSack :: String -> (String, String)
-stringToRuckSack s = splitAt half s
-  where half = length s `div` 2
-
-itemInCommon :: (String, String) -> String
-itemInCommon (left, right) = left `intersect` right
+itemInCommon :: (String, String, String) -> String
+itemInCommon (l, m, r) = [x | x <- l, y <- m, z <- r, (x == y) && (x == z) && (y == z)]
 
 -- | [ord 'a' ... ord 'z'] -> [97, 122] - 95 -> [1, 26]
 -- | [ord 'A' ... ord 'Z'] -> [65, 90] - 38 ->  [27, 52]
@@ -17,9 +13,13 @@ charToScore c
   | isUpper c = ord c - 38
   | otherwise = ord c - 96
 
+stringToGroupOfRucksacks :: [String] -> [(String, String, String)]
+stringToGroupOfRucksacks [] = []
+stringToGroupOfRucksacks (r1:r2:r3:rs) = (r1, r2, r3): stringToGroupOfRucksacks rs
+
 main :: IO ()
 main = do
   content <- lines <$> readFile "input.txt"
-  let ruckSacks = map stringToRuckSack content
-  let prioritiesSum = sum $ map ((charToScore . head) . itemInCommon) ruckSacks
+  let groupOfRucksacks = stringToGroupOfRucksacks content
+  let prioritiesSum = sum $ map ((charToScore . head) . itemInCommon) groupOfRucksacks
   print prioritiesSum
